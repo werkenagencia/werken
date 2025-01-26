f5e7e36171a23e903f091ea8671b8fb30d7bbae6
werkenagencia <werkenagency@gmail.com>
2025-01-26 17:11:04 +0100

    Update menu.js

--- template/Pinko/assets/js/menu.js
+++ template/Pinko/assets/js/menu.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuTrigger = document.querySelector(".menu-trigger");
+  const navMenu = document.querySelector(".nav");
 
+  menuTrigger.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
+  });
+});
