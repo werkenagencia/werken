d609644e6a3c01e1672ea7a633d400d9c45f4da4
werkenagencia <werkenagency@gmail.com>
2025-02-11 16:49:28 +0100

    Delete template/GPbyFer directory

--- template/GPbyFer/assets/js/parallax.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("scroll", function () {
-    let scrollTop = window.scrollY;
-    
-    document.querySelector(".parallax-img.one").style.transform = `translateY(${scrollTop * 0.2}px)`;
-    document.querySelector(".parallax-img.two").style.transform = `translateY(-${scrollTop * 0.2}px)`;
-  });
-  
