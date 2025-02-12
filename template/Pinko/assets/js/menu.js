ed3201b505407214bc1f014bfb3d89d7b0ac55f2
werkenagencia <werkenagency@gmail.com>
2025-02-13 00:54:33 +0100

    Delete template/Pinko directory

--- template/Pinko/assets/js/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuTrigger = document.querySelector(".menu-trigger");
-  const navMenu = document.querySelector(".nav");
-
-  menuTrigger.addEventListener("click", function () {
-    navMenu.classList.toggle("active");
-  });
-});
